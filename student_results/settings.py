e24c383c6456bf7ee9ca399b5e02b1f672c382d6
DEBJIT PAUL <codeknightdebjit@gmail.com>
2026-03-08 13:42:52 +0530

    settings updated

--- student_results/settings.py
+++ student_results/settings.py
@@ -107,9 +107,9 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = ['staticfiles']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Razorpay Configuration
 RAZORPAY_KEY_ID = config('RAZORPAY_KEY_ID', default='')
